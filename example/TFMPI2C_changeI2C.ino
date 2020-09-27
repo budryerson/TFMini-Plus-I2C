8d8b39cd531b241fe801597c73a23fd60ae32bd5
Bud Ryerson <bud@budryerson.com>
2020-09-27 16:32:46 -0700

    Replaced `printf()` with `Serial.print()`
    I tried to replace every instance of `printf()`. Some Arduino's such as the Due and ESP32 are troublesome, and require a CR/LF with every instance.  I thought it might be better without them.  You tell me.

--- example/TFMPI2C_changeI2C.ino
+++ example/TFMPI2C_changeI2C.ino
@@ -1,14 +1,14 @@
 /* File Name: TFMPI2C_changeI2C.ino 
  * Developer: Bud Ryerson
  * Inception: 16 FEB 2020
- * Last work: 03 SEP 2020
+ * Last work: 27 SEP 2020
  *
  * Description:
  *  Run an I2C address search.
  *  Use first address found as old address.
  *  Request new I2C address.
  *  Send command to device at old address to accept new device address.
- *  Wait 5 seconds and restart program loop
+ *  Wait 4 seconds and restart program loop
  */
 
 #include <Wire.h>     // Arduino standard I2C/Two-Wire Library
@@ -25,8 +25,9 @@ uint8_t oldAddr, newAddr;
 
 bool scanAddr()
 {
-    printf("\r\nShow all I2C addresses in Decimal and Hex.");
-    printf("\r\nScanning...\r\n");
+    Serial.println();
+    Serial.println( "Show all I2C addresses in Decimal and Hex.");
+    Serial.println( "Scanning...");
     I2C_total = 0;
     I2C_error = 0;
     oldAddr = 0x10; // default address
@@ -39,24 +40,35 @@ bool scanAddr()
 
         if( I2C_error == 0)
         {
-            printf( "I2C device found at address %03i (0x%02X HEX).\r\n", x, x);
+            Serial.print( "I2C device found at address ");
+            printAddress( x);
             ++I2C_total;   //  Increment for each address returned.
             if( I2C_total == 1) oldAddr = x;
         }
         else if( I2C_error == 4)
         {
-            printf( "Unknown I2C error at address %03i (0x%02X HEX).\r\n", x, x);
+            Serial.print( "Unknown I2C error at address ");
+            Serial.println( x);
         }
     }
     //  Display results and return boolean value.
     if( I2C_total == 0)
     {
-      printf( "No I2C devices found.");
+      Serial.println( "No I2C devices found.");
       return false;
     }
     else return true;
 }
 
+// Print address in decimal and HEX
+void printAddress( uint8_t adr)
+{
+    Serial.print( adr);
+    Serial.print( " (0x");
+    Serial.print( adr < 16 ? "0" : ""); 
+    Serial.print( adr, HEX);
+    Serial.println( " Hex)");
+}
 
 void setup()
 {
@@ -69,10 +81,13 @@ void setup()
     while( Serial.available())Serial.read();  // flush serial read buffer
 
     // Say hello
-    printf( "\r\n*****************************\r\n");
-    printf( "\r\nThis sketch will command a TFMini-Plus device in");
-    printf( "\r\nI2C communications mode to change the I2C address.");
-    printf( "\r\nClose terminal window to halt program loop.\r\n");
+    Serial.println();
+    Serial.println( "*****************************");
+    Serial.println( "Scan I2C bus for all devices and");
+    Serial.println( "display first device address found.");
+    Serial.println( "Enter new address in decimal (or return).");
+    Serial.println( "Confirm 'Y/N' in 5 seconds. Default is 'N'.");
+    Serial.println( "Close window to halt program.");
     delay(1000);
 }
 
@@ -83,57 +98,40 @@ void loop()
      // save first address found as 'old' address.
     if( scanAddr())
     {
-      printf( "\r\nFirst I2C address found: %02i.", oldAddr);
-      printf( "\r\nEnter new address from 1 to 127 decimal (not Hex): ");
+      Serial.print( "\r\nFirst I2C address found: ");
+      printAddress( oldAddr);
+      Serial.print( "Enter new address from 1 to 127 decimal (not Hex): ");
 
       // parse integer from serial port input,
       // recast as a byte and save as 'new' address
       while( Serial.available() == 0);
       newAddr = uint8_t( Serial.parseInt());
-      Serial.println( newAddr);
+      printAddress( newAddr);
 
       // Get Y/N response to continue
-      printf( "Change I2C address from %i to %i: ", oldAddr, newAddr);
+      Serial.print( "Change I2C address from ");
+      Serial.print( oldAddr);
+      Serial.print( " to ");
+      Serial.print( newAddr);
+      Serial.print( " ");
       if( tfmP.getResponse())
       {
-        printf( "\r\n*****************************\r\n");
-        printf( "\r\nSet I2C Address: ");
+        Serial.println( "\r\n*****************************");
+        Serial.print( "Set I2C Address: ");
         //  Send command to change address
         if( tfmP.sendCommand( SET_I2C_ADDRESS, newAddr, oldAddr))
         {
-            printf( "%02i.\r\n", newAddr);
+            printAddress( newAddr);
         }
         else tfmP.printReply();
       }
       else  // If response is "N"
       {
-        printf( "\r\nDo not change I2C address.");
+        Serial.println( "\r\nNo change to I2C address.");
       }
     }
-    printf( "\r\n\nProgram will halt for 4 seeconds and restart.");
-    printf( "\r\n*****************************\r\n");
+    Serial.println( "\r\n\nProgram will halt for 4 seeconds and restart.");
+    Serial.println( "*****************************");
     delay( 4000);           // And wait for 4 seconds
 }
 // = = = = = = = = =  End of Main Loop  = = = = = = = = =
-
-/*
-
-// Test to get data from device in I2C mode
-void getI2CData( uint8_t I2C_addr)
-{
-    // Initialize getI2CData() variables
-    static uint16_t tfDist = 0;       // Distance to object in centimeters
-    static uint16_t tfFlux = 0;       // Signal strength or quality of return signal
-    static uint16_t tfTemp = 0;       // Internal temperature of Lidar sensor chip
-
-    tfmP.getData( tfDist, tfFlux, tfTemp, I2C_addr); // Get a frame of data
-    if( tfmP.status == TFMP_READY)         // If no I2C_err...
-    {
-        printf( "Dist:%04ucm ", tfDist);   // display distance,
-        printf( "Flux:%05u ",   tfFlux);   // display signal strength/quality,
-        printf( "Temp:%2u\°C",  tfTemp);   // display temperature,
-        printf( "\r\n");                   // end-of-line.
-    }
-    else tfmP.printErrorStatus();          // Otherwise, display I2C_err.
-}
-*/

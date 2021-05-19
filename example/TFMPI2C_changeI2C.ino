d4394c34e79174849c8709296790df1ba7e2e4b4
Bud Ryerson <bud@budryerson.com>
2021-05-19 14:57:54 -0700

    Changed version number

--- example/TFMPI2C_changeI2C.ino
+++ example/TFMPI2C_changeI2C.ino
@@ -18,7 +18,7 @@
                       // devices such as the Galileo. Download from:
                       // https://github.com/spaniakos/AES/blob/master/printf.h
 
-#include <TFMPI2C.h>  // TFMini-Plus I2C Library v1.5.0
+#include <TFMPI2C.h>  // TFMini-Plus I2C Library v1.5.1
 TFMPI2C tfmP;         // Create a TFMini-Plus I2C object
 
 // Declare variables
